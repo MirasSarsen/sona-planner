a97e194100a7be1db0a7c8bc1ce3f27a04d8e705
Miras Sarsen <miras.frontender@gmail.com>
2026-03-26 02:23:20 +0500

    upd env

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+const SUPABASE_URL = "https://dnwegznxivvygcbymna.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_OQzebzlrh3NOLkjZ9qZm3A_F77-yhP4";
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
