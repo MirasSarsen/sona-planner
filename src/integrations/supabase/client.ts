618451d6ca839151f497382290f9604da63f558d
Miras Sarsen <miras.frontender@gmail.com>
2026-03-26 10:01:43 +0500

    git upd

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,11 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://dnwegznxivvygcbymna.supabase.co";
+const SUPABASE_URL = "https://dnwegznxixvvycgbymna.supabase.co";
 const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_OQzebzlrh3NOLkjZ9qZm3A_F77-yhP4";
-
-// Import the supabase client like this:
-// import { supabase } from "@/integrations/supabase/client";
 
 export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
   auth: {
